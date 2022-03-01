892582294125c3b30facd5fe860f0bf169ba0917
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-03-01 12:02:17 +0530

    Time: 218 ms (13.83%), Space: 134.4 MB (9.11%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -7,28 +7,43 @@ class Solution {
         ans[n - 1] = 0;
         st.push(n-1);
         
+        // for(int i = n-2; i >= 0; i--)
+        // {
+        //     if(!st.isEmpty() && t[i] < t[st.peek()])
+        //     {
+        //         ans[i] = st.peek() - i;
+        //         st.push(i);
+        //     }
+        //     else{
+        //         while(!st.isEmpty() && t[i] >= t[st.peek()])
+        //         {
+        //             st.pop();
+        //         }
+        //         if(st.isEmpty())
+        //         {
+        //             st.push(i);
+        //             ans[i] = 0;
+        //         }
+        //         else{
+        //             ans[i] = st.peek() - i;
+        //             st.push(i);    
+        //         }
+        //     }
+        // }
+        
         for(int i = n-2; i >= 0; i--)
         {
-            if(!st.isEmpty() && t[i] < t[st.peek()])
+            while(!st.isEmpty() && t[i] >= t[st.peek()])
             {
-                ans[i] = st.peek() - i;
-                st.push(i);
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i] = 0;
             }
             else{
-                while(!st.isEmpty() && t[i] >= t[st.peek()])
-                {
-                    st.pop();
-                }
-                if(st.isEmpty())
-                {
-                    st.push(i);
-                    ans[i] = 0;
-                }
-                else{
-                    ans[i] = st.peek() - i;
-                    st.push(i);    
-                }
+                ans[i] = st.peek() - i;
             }
+            st.push(i);
         }
         return ans;
     }
