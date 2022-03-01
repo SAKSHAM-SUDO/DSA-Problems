17444f332c0d9b35f67ee835523acff4a08326b4
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-03-01 13:37:23 +0530

    Added solution - LeetHub

--- Stock span problem - GFG/stock-span-problem.java
+++ Stock span problem - GFG/stock-span-problem.java
@@ -42,22 +42,18 @@ class Solution
     public static int[] calculateSpan(int price[], int n)
     {
         int ans[] = new int[n];
-        Stack<Integer> st = new Stack<>();
-        st.push(0);
+        // Stack<Integer> st = new Stack<>();
+        // st.push(0);
         ans[0] = 1;
         for(int i = 1; i < n; i++)
         {
-            while(!st.isEmpty() && price[i] >= price[st.peek()])
+            int days = 1;
+            
+            while((i - days) >= 0 && price[i] >= price[i - days])
             {
-                st.pop();
+                days = days + ans[i - days];
             }
-            if(st.isEmpty()){
-                ans[i] = i + 1;
-            }
-            else{
-                ans[i] = i - st.peek();
-            }
-            st.push(i);
+            ans[i] = days;
         }
         return ans;
     }
