237faa87280930e532ea8d5bc947f70f2905ba7f
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-03-18 23:41:07 +0530

    Time: 5 ms (60.22%), Space: 42.7 MB (55.36%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -1,32 +1,32 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
         int freq[] = new int[26];
-        
+        Stack<Character> st = new Stack<>();
+        boolean[] present = new boolean[26];
+        st.push(s.charAt(0));
+        freq[s.charAt(0) - 'a']--;
+        present[s.charAt(0) - 'a'] = true;
         for(int i = 0; i < s.length(); i++)
         {
             freq[s.charAt(i) - 'a']++;
         }
-        boolean[] exist = new boolean[26];
-        Arrays.fill(exist, false);
-        Stack<Character> st = new Stack<>();
         
-        for(int i =0; i < s.length(); i++)
+        for(int i = 1; i < s.length(); i++)
         {
             char ch = s.charAt(i);
             freq[ch - 'a']--;
-            if(!exist[ch - 'a'])
+            if(!present[ch - 'a'])
             {
-                while(!st.isEmpty() && st.peek() > ch && freq[st.peek() - 'a'] != 0)
-                {
-                    char popped = st.pop();
-                    exist[popped - 'a'] = false;
-                }
+               while(!st.isEmpty() && ch < st.peek() && freq[st.peek()- 'a'] != 0)
+               {
+                   present[st.peek()-'a'] = false;
+                   st.pop();
+               }
                 st.push(ch);
-                exist[ch - 'a']  = true;
+                present[ch - 'a'] = true;
             }
         }
-        String ans=  "";
-        
+        String ans = "";
         while(!st.isEmpty())
         {
             ans = st.pop() + ans;
