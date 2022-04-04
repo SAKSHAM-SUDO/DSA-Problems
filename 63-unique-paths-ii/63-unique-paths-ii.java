368dbd4a565b8732cad829ddb9fff8e4906b7d7e
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-04-04 22:49:09 +0530

    Time: 1 ms (54.46%), Space: 42.4 MB (44.20%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -3,37 +3,62 @@ class Solution {
         int r  = obstacleGrid.length;
         int c  = obstacleGrid[0].length;
         int dp[][] = new int[r][c];
-        for(int rows[] : dp)
+        // for(int rows[] : dp)
+        // {
+        //     Arrays.fill(rows, -1);
+        // }
+        // int count = getPaths(0, 0, r, c, obstacleGrid, dp);
+        // return count;
+        
+        for(int i = 0; i < r; i++)
         {
-            Arrays.fill(rows, -1);
+            for(int j = 0; j < c; j++)
+            {
+                if(i == 0 && j == 0 && obstacleGrid[i][j] == 0)
+                {
+                    dp[i][j] = 1;
+                }
+                else if(obstacleGrid[i][j] == 1)
+                {
+                    dp[i][j] = 0;
+                }
+                else{
+                    int left = 0;
+                    int up = 0;
+                    if(j - 1 >= 0)
+                        left = dp[i][j-1];
+                    if(i - 1 >= 0)
+                        up = dp[i-1][j];
+                    dp[i][j] = left+ up;
+                }
+            }
         }
-        int count = getPaths(0, 0, r, c, obstacleGrid, dp);
-        return count;
+        return dp[r-1][c-1];
     }
-    public int getPaths(int i, int j, int r, int c, int grid[][], int dp[][])
-    {
-        if(i == r-1 && j == c - 1)
-        {
-            if(grid[r-1][c-1] == 1)
-            return 0;
-            else
-                return 1;
-        }
-        else if(i >= r || j >= c)
-        {
-            return 0;
-        }
-        else if(grid[i][j] == 1)
-        {
-            return 0;
-        }
-        else if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-        int right = getPaths(i, j + 1, r, c, grid, dp);
-        int down = getPaths(i + 1, j, r, c, grid, dp);
-        dp[i][j] = down + right;
-        return dp[i][j];
-    }
+    // public int getPaths(int i, int j, int r, int c, int grid[][], int dp[][])
+    // {
+    //     if(i == r-1 && j == c - 1)
+    //     {
+    //         if(grid[r-1][c-1] == 1)
+    //         return 0;
+    //         else
+    //             return 1;
+    //     }
+    //     else if(i >= r || j >= c)
+    //     {
+    //         return 0;
+    //     }
+    //     else if(grid[i][j] == 1)
+    //     {
+    //         return 0;
+    //     }
+    //     else if(dp[i][j] != -1)
+    //     {
+    //         return dp[i][j];
+    //     }
+    //     int right = getPaths(i, j + 1, r, c, grid, dp);
+    //     int down = getPaths(i + 1, j, r, c, grid, dp);
+    //     dp[i][j] = down + right;
+    //     return dp[i][j];
+    // }
 }
