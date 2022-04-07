0c8e122fc2961c5f1cb69297a698b274170b3117
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-04-07 20:31:22 +0530

    Time: 2 ms (87.00%), Space: 69 MB (13.46%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,37 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums)
     {
-        int len = nums.length;
-        boolean[] visited = new boolean[len];
-        return check(0, nums, len, visited);
-    }
-    public boolean check(int idx, int nums[], int len, boolean visited[])
-    {
-        if(idx >= len)
+        int goal = nums.length - 1;
+        for(int i = nums.length - 2; i >= 0; i--)
         {
-            return false;
+            if(i + nums[i] >= goal)
+                goal = i;
         }
-        if(idx == len-1)
-        {
+        if(goal == 0)
             return true;
-        }
-        if(visited[idx])
-        {
-            return false;
-        }
-        visited[idx] = true;
-        int maxSteps = nums[idx];
-        if(maxSteps == 0)
-        {
-            return false;
-        }
-            
-        for(int i = 1; i <= maxSteps; i++)
-        {
-            if(check(idx + i, nums, len, visited))
-                return true;
-            
-        }
-        return false;
+        else return false;
     }
 }
