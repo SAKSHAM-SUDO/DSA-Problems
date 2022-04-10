43c1c72d95d771a77a4d0b7f650be595edb0ad71
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-04-10 12:35:07 +0530

    Time: 16 ms (59.99%), Space: 51 MB (20.40%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,34 +1,36 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // O(1) time
-        if (k == nums.length) {
+        if(k == nums.length)
+        {
             return nums;
         }
+        HashMap<Integer, Integer> map = new HashMap<>();
         
-        // 1. build hash map : character and how often it appears
-        // O(N) time
-        Map<Integer, Integer> count = new HashMap();
-        for (int n: nums) {
-          count.put(n, count.getOrDefault(n, 0) + 1);
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(map.containsKey(nums[i]))
+            {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+            else{
+                map.put(nums[i], 1);
+            }
         }
-
-        // init heap 'the less frequent element first'
-        Queue<Integer> heap = new PriorityQueue<>(
-            (n1, n2) -> count.get(n1) - count.get(n2));
-
-        // 2. keep k top frequent elements in the heap
-        // O(N log k) < O(N log N) time
-        for (int n: count.keySet()) {
-          heap.add(n);
-          if (heap.size() > k) heap.poll();    
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (num1, num2) -> map.get(num1) - map.get(num2));
+        
+        for(int num : map.keySet())
+        {
+            pq.add(num);
+            if(pq.size() > k)
+                pq.poll();
         }
-
-        // 3. build an output array
-        // O(k log k) time
-        int[] top = new int[k];
-        for(int i = k - 1; i >= 0; --i) {
-            top[i] = heap.poll();
+        int ans[] = new int[k];
+        for(int i = 0; i < k; i++)
+        {
+            ans[i] = pq.poll();
         }
-        return top;
+        return ans;
     }
 }
