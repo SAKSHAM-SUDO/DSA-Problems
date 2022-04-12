db9e3b2b68882f72c83e34786c44c18142c3f276
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-04-12 17:55:50 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (17.23%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,44 +1,70 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int n = board.length;
-        int m = board[0].length;
-        int prev [][] = new int [n][m];
-        for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                prev[i][j]=board[i][j];
-            }
-        }
-        int population_count [][] = new int [n][m];
-        for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                int count = 0;
-                if(i-1>=0&&j-1>=0&&i-1<n&&j-1<m)count+=board[i-1][j-1];
-                if(i-1>=0&&j>=0&&i-1<n&&j<m)count+=board[i-1][j];
-                if(i-1>=0&&j+1>=0&&i-1<n&&j+1<m)count+=board[i-1][j+1];
-                if(i>=0&&j-1>=0&&i<n&&j-1<m)count+=board[i][j-1];
-                if(i>=0&&j+1>=0&&i<n&&j+1<m)count+=board[i][j+1];
-                if(i+1>=0&&j-1>=0&&i+1<n&&j-1<m)count+=board[i+1][j-1];
-                if(i+1>=0&&j>=0&&i+1<n&&j<m)count+=board[i+1][j];
-                if(i+1>=0&&j+1>=0&&i+1<n&&j+1<m)count+=board[i+1][j+1];
-                population_count[i][j]=count;
-            }
-        }
-        for(int i =0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(prev[i][j]==1){
-                    if(population_count[i][j]<2){
-                        board[i][j]=0;
+        int m = board.length;
+        int n = board[0].length;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int c = count(board, i, j);
+                if(board[i][j] == 1) {
+                    if(c < 2) { 
+                        board[i][j] = -1; // -1 represents live cell becomes dead
                     }
-                    else if(population_count[i][j]>3){
-                        board[i][j]=0;
+                    if(c == 2 || c == 3) {
+                        board[i][j] = 1;
+                    }
+                    if(c > 3) {
+                        board[i][j] = -1;  // -1 represents live cell becomes dead
+                    }
+                } else {
+                    if(c == 3) {
+                        board[i][j] = -2;  // -2 represents died cell becomes live
                     }
                 }
-                else{
-                   if(population_count[i][j]==3){
-                        board[i][j]=1;
-                    } 
+            }
+        }
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == -1) {
+                    board[i][j] = 0; // Restore the value for next stage
+                }
+                if(board[i][j] == -2) {
+                    board[i][j] = 1; // Restore the value for next stage
                 }
             }
         }
+    }
+    // Count live neighbours
+    public int count(int[][] board, int i, int j) {
+        int m = board.length;
+        int n = board[0].length;
+        int count = 0;
+        if(i-1 >= 0 && j-1 >= 0) {
+            if(board[i-1][j-1] == 1 || board[i-1][j-1] == -1) count++;
+        }
+        if(i-1 >= 0 && j >= 0) {
+            if(board[i-1][j] == 1 || board[i-1][j] == -1) count++;
+        }
+        if(i-1 >= 0 && j+1 < n) {
+            if(board[i-1][j+1] == 1 || board[i-1][j+1] == -1) count++;
+        }
+        
+        if(i >= 0 && j-1 >= 0) {
+            if(board[i][j-1] == 1 || board[i][j-1] == -1) count++;
+        }
+        if(i >= 0 && j+1 < n) {
+            if(board[i][j+1] == 1 || board[i][j+1] == -1) count++;
+        }
+        
+        if(i+1 < m && j-1 >= 0) {
+            if(board[i+1][j-1] == 1 || board[i+1][j-1] == -1) count++;
+        }
+        if(i+1 < m && j >= 0) {
+            if(board[i+1][j] == 1 || board[i+1][j] == -1) count++;
+        }
+        if(i+1 < m && j+1 < n) {
+            if(board[i+1][j+1] == 1 || board[i+1][j+1] == -1) count++;
+        }
+        return count;
     }
 }
