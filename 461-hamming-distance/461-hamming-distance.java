3592e75e546211cacf9f014d0a248c7a9749c3f9
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-04-26 10:50:01 +0530

    Time: 1 ms (35.03%), Space: 41.1 MB (44.51%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int c = 0;
+        int res = x ^ y;
+        while(res != 0)
+        {
+            res = res & (res - 1);
+            c++;
+        }
+        return c;
+    }
+}
