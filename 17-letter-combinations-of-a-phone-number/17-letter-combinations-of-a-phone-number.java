5eed8e6863c0a7964aa02f0817645600914a0a95
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-05-09 19:21:32 +0530

    Time: 1 ms (91.35%), Space: 42.7 MB (55.51%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,27 +1,37 @@
 class Solution {
-    String arr[] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    final static HashMap<Character,String> map=new HashMap<>(){{
+        put('2',"abc");
+        put('3',"def");
+        put('4',"ghi");
+        put('5',"jkl");
+        put('6',"mno");
+        put('7',"pqrs");
+        put('8',"tuv");
+        put('9',"wxyz");
+    }};
+
     public List<String> letterCombinations(String digits) {
         if(digits.length() == 0)
-            return new  ArrayList<>();
-        ArrayList<String> ans = new ArrayList<>();
-        ans = calc("", digits, digits.length());
-        return ans;
+            return new ArrayList<>();
+        
+        ArrayList<String> list = new ArrayList<>();
+        backtrack(new StringBuilder(), digits, list, digits.length());
+        return list;
     }
-    public ArrayList<String> calc(String res, String digits, int len)
+    public void backtrack(StringBuilder res, String digits, ArrayList<String> list, int len)
     {
         if(res.length() == len)
         {
-            ArrayList<String> l = new ArrayList<>();
-            l.add(res);
-            return l;
+            list.add(res.toString());
+            return;
         }
-        int index = (digits.charAt(0) - '0') - 2;
-        String word = arr[index];
-        ArrayList<String> list = new ArrayList<>();
-        for(int j = 0; j < word.length(); j++)
+        char ch = (digits.charAt(0));
+        String word = map.get(ch);
+        for(int i = 0; i < word.length(); i++)
         {
-            list.addAll(calc(res + word.charAt(j), digits.substring(1), len));
+            res.append(word.charAt(i));
+            backtrack(res, digits.substring(1), list, len);
+            res.deleteCharAt(res.length()-1);
         }
-        return list;
     }
 }
