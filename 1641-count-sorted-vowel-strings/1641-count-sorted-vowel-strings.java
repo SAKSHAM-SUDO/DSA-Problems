9ec2a1adf6f2496662ae44c810501621473101dd
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-05-11 19:51:10 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (61.67%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,23 +1,32 @@
 class Solution {
     public int countVowelStrings(int n) {
-        int count = 0;
-        count = calc(0, n, 0);
-        return count;
-    }
-    public int calc(int idx, int n, int count)
-    {
-        if(count == n)
-        {
-            return 1;
-        }
-        else if(count > n)
-            return 0;
+        // int count = 0;
+        // count = calc(0, n, 0);
+        // return count;
+        int[][] dp=new int[n+1][6];
         
-        int total = 0;
-        for(int i = idx; i < 5; i++)
-        {
-            total = total + calc(i, n, count + 1);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=5;j++){
+                dp[i][j]=dp[i][j-1]+(i>1?dp[i-1][j]:1);
+            }
         }
-        return total;
+        
+        return dp[n][5];
     }
+//     public int calc(int idx, int n, int count)
+//     {
+//         if(count == n)
+//         {
+//             return 1;
+//         }
+//         else if(count > n)
+//             return 0;
+        
+//         int total = 0;
+//         for(int i = idx; i < 5; i++)
+//         {
+//             total = total + calc(i, n, count + 1);
+//         }
+//         return total;
+//     }
 }
