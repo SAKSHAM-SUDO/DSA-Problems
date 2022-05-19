f82763bccc72259eb86840aa50603e1d12cc6fc5
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-05-19 23:21:25 +0530

    Time: 0 ms (100.00%), Space: 41 MB (82.22%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -13,46 +13,21 @@
  *     }
  * }
  */
-class Pair
-{
-    TreeNode node;
-    int lvl;
-    Pair(TreeNode node, int lvl)
-    {
-        this.node = node;
-        this.lvl = lvl;
-    }
-}
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        if(root == null)
-            return new ArrayList<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        Queue<Pair> q = new LinkedList<>();
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        q.add(new Pair(root, 0));
-        while(!q.isEmpty())
-        {
-            Pair pair = q.poll();
-            TreeNode node = pair.node;
-            int lvl = pair.lvl;
-            
-            if(map.containsKey(lvl))
-            {
-                map.put(lvl, node.val);
-            }
-            else
-                map.put(lvl, node.val);
-            
-            if(node.left != null)
-                q.add(new Pair(node.left, lvl + 1));
-            if(node.right != null)
-                q.add(new Pair(node.right, lvl + 1));
-        }
-        for(Map.Entry<Integer, Integer> entry : map.entrySet())
-        {
-            list.add(entry.getValue());
-        }
+        List<Integer> list = new ArrayList<>();
+        func(root, 0, list);
         return list;
     }
+    public void func(TreeNode node, int level, List<Integer> list)
+    {
+        if(node == null)
+            return;
+        if(level == list.size())
+            list.add(node.val);
+        
+        func(node.right, level + 1, list);
+        func(node.left, level + 1, list);
+    }
+    
 }
