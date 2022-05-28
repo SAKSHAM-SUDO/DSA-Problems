5284aa78d050913efe8b69b048e36d9fdef4097c
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-05-28 19:50:18 +0530

    Time: 1 ms (65.97%), Space: 51.3 MB (36.97%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        // int n = nums.length;
+        // int sum = n*(n+1)/2;
+        // for(int i= 0; i<n; i++)
+        // {
+        //     sum = sum - nums[i];
+        // }
+        // return sum;
+        
+        int i = 0;
+        
+        while(i<nums.length)
+        {
+            int correct = nums[i];
+            if(nums[i] < nums.length && nums[i] != nums[correct])
+            {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            
+            else 
+                i++;
+        }
+        for(int j =0; j<nums.length; j++)
+        {
+            if(j != nums[j])
+                return j;
+        }
+        
+        return nums.length;
+    }
+}
