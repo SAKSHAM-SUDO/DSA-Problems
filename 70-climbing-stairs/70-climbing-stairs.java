12f3b4034d48d3f3bb9ffc7e36d9d9506b32c478
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-06-21 11:36:54 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (21.38%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        dp[1] = 1;
+        return calc(n, dp);
+    }
+    public int calc(int n, int dp[])
+    {
+        if(n==0 || n==1) return 1;
+
+        if(dp[n] != -1)
+            return dp[n];
+        
+        else
+        {
+            return dp[n] = calc(n-1, dp) + calc(n-2, dp);            
+        }
+
+    }
+}
