b8269dd1f2f067132b60bc1c30fe4e9227f0ee30
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-07-03 18:33:22 +0530

    Time: 1 ms (33.29%), Space: 41.6 MB (51.43%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -9,28 +9,49 @@ class Solution {
             return nums.length;
         }
 
-        int p = count(nums, 1, false);
-        int c = count(nums, 1, true);
+        int dp[][] = new int[nums.length][2];
+        for(int row[] : dp)
+        {
+            Arrays.fill(row, -1);
+        }
+        int p = count(nums, 1, 0, dp);
+        int c = count(nums, 1, 1, dp);
         return 1 + (int)Math.max(p, c);
     }
-    public int count(int nums[], int idx, boolean pos)
+    public int count(int nums[], int idx, int pos, int dp[][])
     {
         if(idx >= nums.length)
             return 0;
         
-        if(pos)
+        if(dp[idx][pos] != -1)
+            return dp[idx][pos];
+        
+        if(pos == 1)
         {
             if(nums[idx - 1] < nums[idx])
-                return 1 + count(nums, idx + 1, false);
+            {
+                dp[idx][pos] = 1 + count(nums, idx + 1, 0, dp);
+                return dp[idx][pos];
+            }
+                
             else
-                return count(nums, idx + 1, true);
+            {
+                dp[idx][pos] =  count(nums, idx + 1, 1, dp);
+                return dp[idx][pos];
+            }
         }
         else
         {
             if(nums[idx - 1] > nums[idx])
-                return 1 + count(nums, idx + 1, true);
+            {
+                dp[idx][pos] =  1 + count(nums, idx + 1, 1, dp);
+                return dp[idx][pos];
+            }
             else
-                return count(nums, idx + 1, false);
+            {
+                dp[idx][pos] = count(nums, idx + 1, 0, dp);
+                return dp[idx][pos];
+            }
             
         }
     }
