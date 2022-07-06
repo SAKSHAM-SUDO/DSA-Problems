7c907d5a045b548d43da09f46e0d4b635bca97b3
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-07-06 23:29:10 +0530

    Time: 0 ms (100.00%), Space: 41 MB (41.71%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,14 +1,21 @@
 class Solution {
     public int fib(int n) {
-         if(n<0){
-            return 0;
-        }
-        if(n == 1 || n == 2)
+        int[] dp = new int[n + 1];
+        if(n != 0)
         {
-            return 1;
+            dp[0] = 0;
+            dp[1] = 1;
+            for(int i = 2 ; i < n+1 ; i++)
+            {
+                int tot = dp[i - 1] + dp[i - 2];
+                dp[i] = tot;
+            }
         }
-        else{
-            return fib(n - 1) + fib(n - 2); 
+        else
+        {
+            dp[0] = 0;
         }
+        return dp[n];
     }
 }
+
