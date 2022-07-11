5c9c7a6ec22ea9a241bed9df70936b3e00de54f7
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-07-11 12:48:33 +0530

    Time: 4 ms (11.45%), Space: 43.5 MB (5.70%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -1,33 +1,43 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+class Pair
+{
+    TreeNode node;
+    int lvl;
+    Pair(TreeNode node, int lvl)
+    {
+        this.node = node;
+        this.lvl = lvl;
+    }
+}
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        func(root, 0, list);
+        if(root == null)
+            return new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<Pair> q = new LinkedList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        q.add(new Pair(root, 0));
+        while(!q.isEmpty())
+        {
+            Pair pair = q.poll();
+            TreeNode node = pair.node;
+            int lvl = pair.lvl;
+            
+            if(map.containsKey(lvl))
+            {
+                map.put(lvl, node.val);
+            }
+            else
+                map.put(lvl, node.val);
+            
+            if(node.left != null)
+                q.add(new Pair(node.left, lvl + 1));
+            if(node.right != null)
+                q.add(new Pair(node.right, lvl + 1));
+        }
+        for(Map.Entry<Integer, Integer> entry : map.entrySet())
+        {
+            list.add(entry.getValue());
+        }
         return list;
     }
-    public void func(TreeNode node, int level, List<Integer> list)
-    {
-        if(node == null)
-            return;
-        if(level == list.size())
-            list.add(node.val);
-        
-        func(node.right, level + 1, list);
-        func(node.left, level + 1, list);
-    }
-    
 }
