3d4efea3747227d5fac00f7654668b3c214d8340
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-07-11 12:57:15 +0530

    Time: 1 ms (95.06%), Space: 43 MB (31.99%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -1,43 +1,32 @@
-class Pair
-{
-    TreeNode node;
-    int lvl;
-    Pair(TreeNode node, int lvl)
-    {
-        this.node = node;
-        this.lvl = lvl;
-    }
-}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        if(root == null)
-            return new ArrayList<>();
         ArrayList<Integer> list = new ArrayList<>();
-        Queue<Pair> q = new LinkedList<>();
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        q.add(new Pair(root, 0));
-        while(!q.isEmpty())
-        {
-            Pair pair = q.poll();
-            TreeNode node = pair.node;
-            int lvl = pair.lvl;
-            
-            if(map.containsKey(lvl))
-            {
-                map.put(lvl, node.val);
-            }
-            else
-                map.put(lvl, node.val);
-            
-            if(node.left != null)
-                q.add(new Pair(node.left, lvl + 1));
-            if(node.right != null)
-                q.add(new Pair(node.right, lvl + 1));
-        }
-        for(Map.Entry<Integer, Integer> entry : map.entrySet())
-        {
-            list.add(entry.getValue());
-        }
+        dfs(root, 0, list);
         return list;
+    }
+    public void dfs(TreeNode curr, int level, ArrayList<Integer> list)
+    {
+        if(curr == null)
+            return;
+        if(list.size() == level)
+            list.add(curr.val);
+        
+        dfs(curr.right, level + 1, list);
+        dfs(curr.left, level + 1, list);
     }
 }
