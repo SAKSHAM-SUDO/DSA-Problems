46a4b008e53e7d6f39d5b4f6f87e59b8f30dddff
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-07-13 00:16:33 +0530

    Time: 1 ms (52.88%), Space: 40.7 MB (69.77%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,27 +1,33 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
-        for(int row[] : dp)
+        dp[0][0] = 1;
+        for(int i = 0; i < m; i++)
         {
-            Arrays.fill(row, -1);
+            for(int j = 0; j < n; j++)
+            {
+                if(i - 1 >= 0)
+                    dp[i][j] += dp[i - 1][j];
+                if(j - 1 >= 0)
+                 dp[i][j] += dp[i][j - 1];
+            }
         }
-        int count = rec(m - 1, n - 1, dp);
-        return count;
+        return dp[m - 1][n - 1];
     }
-    public int rec(int m, int n, int dp[][])
-    {
-        if(m == 0 && n == 0)
-            return 1;
-        else if(m < 0 || n < 0)
-            return 0;
+//     public int rec(int m, int n, int dp[][])
+//     {
+//         if(m == 0 && n == 0)
+//             return 1;
+//         else if(m < 0 || n < 0)
+//             return 0;
         
-        if(dp[m][n] != -1)
-            return dp[m][n];
+//         if(dp[m][n] != -1)
+//             return dp[m][n];
         
-        int left = rec(m-1, n, dp);
-        int right = rec(m, n-1, dp);
+//         int left = rec(m-1, n, dp);
+//         int right = rec(m, n-1, dp);
         
-        dp[m][n] = left + right;
-        return dp[m][n];
-    }
+//         dp[m][n] = left + right;
+//         return dp[m][n];
+//     }
 }
