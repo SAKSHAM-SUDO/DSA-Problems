a07de241c567c00d08ec8876712c673be54f4e62
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-07-14 16:08:54 +0530

    Time: 70 ms (47.98%), Space: 52.1 MB (70.14%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -7,28 +7,44 @@ class Solution {
         if(sum % 2 == 1)
             return false;
         
-        Boolean dp[][] = new Boolean[nums.length + 1][sum/2 + 1];
-        
-        return rec(nums, nums.length - 1, sum / 2, dp);
-    }
-    public boolean rec(int nums[], int idx, int target,Boolean dp[][])
-    {
-        if(idx == 0)
-            return target == nums[0];
-        
-        else if(target < 0)
-            return false;
-        
-        else if(target == 0)
+        boolean dp[][] = new boolean[nums.length][sum/2 + 1];
+        for(int i = 0; i < nums.length; i++)
         {
-            dp[idx][target] = true;
-            return true;
+            dp[i][0] = true;
         }
-            
-        else if(dp[idx][target] != null)
-            return dp[idx][target];
-        
-        dp[idx][target] = rec(nums, idx - 1, target - nums[idx], dp) || rec(nums, idx - 1, target,                                                                                              dp);
-        return dp[idx][target];
+        if(nums[0] <= sum/2)
+        dp[0][nums[0]] = true;
+        for(int i = 1; i < dp.length; i++)
+        {
+            for(int j = 1;j < dp[0].length; j++)
+            {
+                boolean notTake = dp[i - 1][j];
+                boolean take = false;
+                if(nums[i] <= j)
+                    take = dp[i - 1][j - nums[i]];
+                dp[i][j] = take | notTake ;                
+            }
+        }
+        return dp[dp.length - 1][sum/2];
     }
+//     public boolean rec(int nums[], int idx, int target,Boolean dp[][])
+//     {
+//         if(idx == 0)
+//             return target == nums[0];
+        
+//         else if(target < 0)
+//             return false;
+        
+//         else if(target == 0)
+//         {
+//             dp[idx][target] = true;
+//             return true;
+//         }
+            
+//         else if(dp[idx][target] != null)
+//             return dp[idx][target];
+        
+//         dp[idx][target] = rec(nums, idx - 1, target - nums[idx], dp) || rec(nums, idx - 1, target,                                                                                              dp);
+//         return dp[idx][target];
+//     }
 }
