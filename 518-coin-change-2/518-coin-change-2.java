895d3b3bec97464df4f35d547c2389c6d306c21e
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-07-15 23:42:37 +0530

    Time: 13 ms (47.67%), Space: 51 MB (33.85%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -3,43 +3,60 @@ class Solution {
     {
         int dp[][] = new int[coins.length][amount + 1];
         
-        for(int row[] : dp)
-            Arrays.fill(row, -1);
-        
-        return rec(coins, coins.length - 1, amount, dp);
-    }
-    public int rec(int coins[], int idx, int amount, int dp[][])
-    {
-        if(amount == 0)
+        for(int i = 0; i < coins.length; i++)
         {
-            dp[idx][amount] = 1;
-            return 1;
+            dp[i][0] = 1;
         }
-        
-        if(idx == 0)
+        for(int i = 0; i <= amount; i++)
         {
-            if(amount % coins[idx] == 0)
+            dp[0][i] = i % coins[0] == 0 ? 1 : 0;
+        }
+        for(int i = 1; i < coins.length; i++)
+        {
+            for(int j = 0; j <= amount; j++)
             {
-                dp[idx][amount] = 1;
-               return 1; 
-            }
-                
-            else
-            {
-                dp[idx][amount] = 0;
-                return 0;
+                int take = 0;
+                int notTake = 0;
+                notTake = dp[i- 1][j];
+                if(j >= coins[i])
+                    take = dp[i][j - coins[i]];
+
+                dp[i][j] = take + notTake;
             }
         }
-        if(dp[idx][amount] != -1)
-            return dp[idx][amount];
-        
-        int take = 0;
-        int notTake = 0;
-        notTake = rec(coins, idx - 1, amount, dp);
-        if(amount >= coins[idx])
-            take = rec(coins, idx, amount - coins[idx], dp);
-        
-        dp[idx][amount] = take + notTake;
-        return dp[idx][amount];
+        return dp[coins.length - 1][amount];
     }
+//     public int rec(int coins[], int idx, int amount, int dp[][])
+//     {
+//         if(amount == 0)
+//         {
+//             dp[idx][amount] = 1;
+//             return 1;
+//         }
+        
+//         if(idx == 0)
+//         {
+//             if(amount % coins[idx] == 0)
+//             {
+//                 dp[idx][amount] = 1;
+//                return 1; 
+//             }
+//             else
+//             {
+//                 dp[idx][amount] = 0;
+//                 return 0;
+//             }
+//         }
+//         if(dp[idx][amount] != -1)
+//             return dp[idx][amount];
+        
+//         int take = 0;
+//         int notTake = 0;
+//         notTake = rec(coins, idx - 1, amount, dp);
+//         if(amount >= coins[idx])
+//             take = rec(coins, idx, amount - coins[idx], dp);
+        
+//         dp[idx][amount] = take + notTake;
+//         return dp[idx][amount];
+//     }
 }
