095e30d67fe5aaf170e6279bb69d1a9da760b730
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-07-15 15:07:43 +0530

    Time: 108 ms (10.85%), Space: 54.4 MB (20.49%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,23 +1,46 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int dp[][] = new int[coins.length + 1][amount+1];
-        for(int i = 0; i < dp.length; i++) {
-            for(int j = 0; j < dp[0].length; j++) {
-                if(j == 0) {
-                    dp[i][j] = 0;
-                }
-                else if(i == 0) {
-                    dp[i][j] = (int)1e5; 
-                }
-                else if(coins[i-1] > j) {
-                    dp[i][j] = dp[i-1][j];
-                }
-                else {
-                    dp[i][j] = Math.min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
-                }
+    public int coinChange(int[] coins, int amount)
+    {
+        if(amount == 0)
+            return 0;
+        int dp[][] = new int[coins.length][amount + 1];
+        
+        for(int row[] : dp)
+            Arrays.fill(row, -1);
+        
+        int ans = rec(coins, coins.length - 1, amount, dp);
+        
+        if(ans == (int)Math.pow(10, 9))
+            return -1;
+        return ans;
+    }
+    public int rec(int coins[], int idx, int amount, int dp[][])
+    {        
+        if(idx == 0)
+        {
+            if(amount % coins[0] == 0)
+            {
+                dp[idx][amount] = (amount / coins[0]);
+                return dp[idx][amount];
+            }
+            else
+            {
+                dp[idx][amount] = (int)Math.pow(10, 9);
+                return dp[idx][amount];
             }
         }
-        return dp[coins.length][amount] > (int)1e4 ? -1:dp[coins.length][amount];
+        else if(dp[idx][amount] != -1)
+            return dp[idx][amount];
+
+        int pick = (int)Math.pow(10, 9);
+        int notPick;
+        
+        notPick = 0 + rec(coins, idx - 1, amount, dp);
+        if(coins[idx] <= amount)
+            pick = 1 + rec(coins, idx, amount - coins[idx], dp);
+        
+        dp[idx][amount] = Math.min(pick, notPick);
+        
+        return dp[idx][amount];
     }
 }
-
