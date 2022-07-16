d4038b63ab863520e714cceecc2a9ff67e2db56d
Saksham Garg <54800729+SAKSHAM-SUDO@users.noreply.github.com>
2022-07-16 13:42:31 +0530

    Time: 17 ms (73.02%), Space: 46.4 MB (53.10%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,30 +1,46 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2)
     {
-        int dp[][] = new int[text1.length()][text2.length()];
-        
-        for(int row[] : dp)
-            Arrays.fill(row, -1);
-        return rec(text1, text2, text1.length() - 1, text2.length() - 1, dp);
-    }
-    public int rec(String text1, String text2, int idx1, int idx2, int dp[][])
-    {
-        if(idx1 < 0 || idx2 < 0)
-            return 0;
-        
-        if(dp[idx1][idx2] != -1)
-            return dp[idx1][idx2];
-        if(text1.charAt(idx1) == text2.charAt(idx2))
+        int dp[][] = new int[text1.length() + 1][text2.length() + 1];
+                
+        for(int i = 1; i <= text1.length(); i++)
         {
-            dp[idx1][idx2] = 1 + rec(text1, text2, idx1 - 1, idx2 - 1, dp);
-            return dp[idx1][idx2];
+            for(int j = 1; j <= text2.length(); j++)
+            {
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+                
+                if(text1.charAt(i - 1) == text2.charAt(j - 1))
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else 
+                {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
+        
+        return dp[text1.length()][text2.length()];
+    }
+//     public int rec(String text1, String text2, int idx1, int idx2, int dp[][])
+//     {
+//         if(idx1 < 0 || idx2 < 0)
+//             return 0;
+        
+//         if(dp[idx1][idx2] != -1)
+//             return dp[idx1][idx2];
+//         if(text1.charAt(idx1) == text2.charAt(idx2))
+//         {
+//             dp[idx1][idx2] = 1 + rec(text1, text2, idx1 - 1, idx2 - 1, dp);
+//             return dp[idx1][idx2];
+//         }
             
         
-        else 
-        {
-            dp[idx1][idx2] = Math.max(rec(text1, text2, idx1 - 1, idx2, dp), rec(text1, text2, idx1, idx2 - 1, dp));
-            return dp[idx1][idx2];
-        }
-    }
+//         else 
+//         {
+//             dp[idx1][idx2] = Math.max(rec(text1, text2, idx1 - 1, idx2, dp), rec(text1, text2, idx1, idx2 - 1, dp));
+//             return dp[idx1][idx2];
+//         }
+//     }
 }
